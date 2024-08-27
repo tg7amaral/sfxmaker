99ea237fba70616940203280a5a6a08cefe7a3a9
Tiago <118070875+tiago-xeke@users.noreply.github.com>
2024-08-27 11:41:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const canvas = document.getElementById('waterCanvas');
-const ctx = canvas.getContext('2d');
-canvas.width = canvas.offsetWidth;
-canvas.height = canvas.offsetHeight;
+const canvas1 = document.getElementById('waterCanvas');
+const ctx1 = canvas1.getContext('2d');
+canvas1.width = canvas1.offsetWidth;
+canvas1.height = canvas1.offsetHeight;
 
 const waves = [];
 const waveCount = 20;
@@ -12,8 +12,8 @@ function getRandom(min, max) {
 
 for (let i = 0; i < waveCount; i++) {
     waves.push({
-        y: (canvas.height / 2) - 10 + Math.random() * 20, // 中央付近に分布
-        length: 0.01 + getRandom(1,2) / 1000000,
+        y: (canvas1.height / 2) - 10 + Math.random() * 20, // 中央付近に分布
+        length: 0.01 + getRandom(1,1) / 1000,
         amplitude: 0.1 + getRandom(70,75),
         frequency: 0.01 + getRandom(1,3) / 250,
         phase: Math.random() * Math.PI * 2
@@ -42,24 +42,24 @@ function getRandomCoords(xmin, xmax,ymin,ymax, propNumberx,propNumbery) {
 
 function animate() {
     requestAnimationFrame(animate);
-    ctx.fillStyle = "#0000001A"; // 透明度を0.1に調整
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx1.fillStyle = "#0000001A"; // 透明度を0.1に調整
+    ctx1.fillRect(0, 0, canvas1.width, canvas1.height);
 
     for(var j = 0; j < 10; j++){
-        let coords = getRandomCoords(0,canvas.width,0, canvas.height,14,7)
-            ctx.fillStyle = j % 2 === 1 ? "#0055FF" : "#FFFFFF";
-            ctx.fillRect(coords.x,coords.y,1,1)
+        let coords = getRandomCoords(0,canvas1.width,0, canvas1.height,14,7)
+            ctx1.fillStyle = j % 2 === 1 ? "#0055FF" : "#FFFFFF";
+            ctx1.fillRect(coords.x,coords.y,1,1)
         }
 
     waves.forEach((wave, index) => {
-        ctx.beginPath();
-        ctx.moveTo(0, wave.y);
-        for (let i = 0; i < canvas.width; i++) {
+        ctx1.beginPath();
+        ctx1.moveTo(0, wave.y);
+        for (let i = 0; i < canvas1.width; i++) {
             const yOffset = Math.sin(i * wave.length + wave.phase) * wave.amplitude * Math.sin(wave.phase);
-            ctx.lineTo(i, wave.y + yOffset);
+            ctx1.lineTo(i, wave.y + yOffset);
         }
-        ctx.strokeStyle = `hsl(${index / 2 + 220}, 100%, 50%)`;
-        ctx.stroke();
+        ctx1.strokeStyle = `hsl(${index / 2 + 220}, 100%, 50%)`;
+        ctx1.stroke();
 
         wave.phase += wave.frequency;
     });
@@ -68,10 +68,10 @@ function animate() {
 animate();
 
 window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas1.width = window.innerWidth;
+    canvas1.height = window.innerHeight;
     waves.forEach((wave) => {
-        wave.y = (canvas.height / 2) - 100 + Math.random() * 200; // 中央付近に再配置
+        wave.y = (canvas1.height / 2) - 100 + Math.random() * 200; // 中央付近に再配置
     });
 });
 
@@ -144,7 +144,204 @@ function playMusic() {
 function yes(){
     navigator.vibrate([200,100,200]);
 
-    setTimeout(function(){            
-        window.location.href = `instagram://user?username=${encodeURIComponent("tiago.invictus")}`;
-    },1500);
+    canvas.style.display = "block";
+
+    if (!disabled) {
+    disabled = true
+    // Loading stage
+    setTimeout(() => {
+      // Completed stage
+      setTimeout(() => {
+        window.initBurst()
+        render()
+        setTimeout(() => {
+          // Reset button so user can select it again
+          disabled = false;
+
+          window.location.href = `instagram://user?username=${encodeURIComponent("tiago.invictus")}`;
+        }, 3000)
+      }, 300)
+    }, 1000)
+  }
 }
+
+// quantidade para adicionar em cada pressionar de botão
+const confettiCount = 100
+const sequinCount = 20
+
+// variáveis de "física"
+const gravityConfetti = 0.3
+const gravitySequins = 0.55
+const dragConfetti = 0.075
+const dragSequins = 0.02
+const terminalVelocity = 3
+
+var disabled = false
+const canvas = document.getElementById('confetti')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let button = document.querySelector(".liquidButton")
+
+// adicionar objetos Confetto/Sequin a arrays para desenhá-los
+let confetti = []
+let sequins = []
+
+// cores, o lado de trás é mais escuro para o efeito de "flip" do confete
+const colors = [
+  { front: '#0077FF', back: '#0066FF' }, // Azul Claro
+  { front: '#0055FF', back: '#0044FF' }, // Azul Médio
+  { front: '#0022CC', back: '#0011BB' }  // Azul Escuro
+]
+
+// função auxiliar para escolher um número aleatório dentro de um intervalo
+randomRange = (min, max) => Math.random() * (max - min) + min
+
+// função auxiliar para obter velocidades iniciais para confetes
+// esta distribuição ponderada ajuda os confetes a parecerem mais realistas
+initConfettoVelocity = (xRange, yRange) => {
+  const x = randomRange(xRange[0], xRange[1])
+  const range = yRange[1] - yRange[0] + 1
+  let y = yRange[1] - Math.abs(randomRange(0, range) + randomRange(0, range) - range)
+  if (y >= yRange[1] - 1) {
+    // Ocasionalmente um confete vai mais alto que o máximo
+    y += (Math.random() < .25) ? randomRange(1, 3) : 0
+  }
+  return { x: x, y: -y }
+}
+
+// Classe Confetto
+function Confetto() {
+  this.randomModifier = randomRange(0, 99)
+  this.color = colors[Math.floor(randomRange(0, colors.length))]
+  this.dimensions = {
+    x: randomRange(5, 9),
+    y: randomRange(8, 15),
+  }
+  let btncoords = button.getBoundingClientRect();
+  this.position = {
+    x: randomRange(btncoords.left, btncoords.right),
+    y: randomRange(btncoords.top, btncoords.bottom),
+  }
+  this.rotation = randomRange(0, 2 * Math.PI)
+  this.scale = {
+    x: 1,
+    y: 1,
+  }
+  this.velocity = initConfettoVelocity([-9, 9], [6, 11])
+}
+Confetto.prototype.update = function() {
+  // aplicar forças à velocidade
+  this.velocity.x -= this.velocity.x * dragConfetti
+  this.velocity.y = Math.min(this.velocity.y + gravityConfetti, terminalVelocity)
+  this.velocity.x += Math.random() > 0.5 ? Math.random() : -Math.random()
+  
+  // definir posição
+  this.position.x += this.velocity.x
+  this.position.y += this.velocity.y
+
+  // girar confete escalando y e definir a cor, 0.09 apenas desacelera a frequência do cosseno
+  this.scale.y = Math.cos((this.position.y + this.randomModifier) * 0.09)    
+}
+
+// Classe Sequin
+function Sequin() {
+  this.color = colors[Math.floor(randomRange(0, colors.length))].back;
+  this.radius = randomRange(1, 2);
+  let btncoords = button.getBoundingClientRect();
+  this.position = {
+    x: randomRange(btncoords.left, btncoords.right),
+    y: randomRange(btncoords.top, btncoords.bottom),
+  },
+  this.velocity = {
+    x: randomRange(-6, 6),
+    y: randomRange(-8, -12)
+  }
+}
+Sequin.prototype.update = function() {
+  // aplicar forças à velocidade
+  this.velocity.x -= this.velocity.x * dragSequins
+  this.velocity.y = this.velocity.y + gravitySequins
+  
+  // definir posição
+  this.position.x += this.velocity.x
+  this.position.y += this.velocity.y   
+}
+
+// adicionar elementos aos arrays para serem desenhados
+initBurst = () => {
+  for (let i = 0; i < confettiCount; i++) {
+    confetti.push(new Confetto())
+  }
+  for (let i = 0; i < sequinCount; i++) {
+    sequins.push(new Sequin())
+  }
+}
+
+// desenhar os elementos na tela
+render = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  
+  confetti.forEach((confetto, index) => {
+    let width = (confetto.dimensions.x * confetto.scale.x)
+    let height = (confetto.dimensions.y * confetto.scale.y)
+    
+    // mover canvas para a posição e girar
+    ctx.translate(confetto.position.x, confetto.position.y)
+    ctx.rotate(confetto.rotation)
+
+    // atualizar valores de "física" do confete
+    confetto.update()
+    
+    // obter a cor da frente ou de trás
+    ctx.fillStyle = confetto.scale.y > 0 ? confetto.color.front : confetto.color.back
+    
+    // desenhar confete
+    ctx.fillRect(-width / 2, -height / 2, width, height)
+    
+    // resetar matriz de transformação
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+  })
+
+  sequins.forEach((sequin, index) => {  
+    // mover canvas para a posição
+    ctx.translate(sequin.position.x, sequin.position.y)
+    
+    // atualizar valores de "física" do sequin
+    sequin.update()
+    
+    // definir a cor
+    ctx.fillStyle = sequin.color
+    
+    // desenhar sequin
+    ctx.beginPath()
+    ctx.arc(0, 0, sequin.radius, 0, 2 * Math.PI)
+    ctx.fill()
+
+    // resetar matriz de transformação
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+  })
+
+  // remover confetes e sequins que caem fora da tela
+  // deve ser feito em loops separados para evitar cintilação perceptível
+  confetti.forEach((confetto, index) => {
+    if (confetto.position.y >= canvas.height) confetti.splice(index, 1)
+  })
+  sequins.forEach((sequin, index) => {
+    if (sequin.position.y >= canvas.height) sequins.splice(index, 1)
+  })
+
+  window.requestAnimationFrame(render)
+}
+
+// re-inicializar canvas se o tamanho da janela mudar
+resizeCanvas = () => {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+}
+
+// ouvinte de redimensionamento
+window.addEventListener('resize', () => {
+  resizeCanvas()
+})
